b8217ed765034ad33746311120bc7e2d04ea2f69
Vineet Sharma <sharma_vineet@apple.com>
2018-10-21 15:43:28 -0700

    FOURTH COMMIT

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 
 app = Flask(__name__)
 
-# list of cat images
+# list of cat images from buzzfeed
 images = [
     "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
     "https://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
